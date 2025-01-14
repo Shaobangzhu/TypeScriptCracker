abfebaa1b9ff34907914dead8da26702742d1c9e
Chaoran Lu <andyluloveusc@gmail.com>
2025-01-13 22:32:05 -0800

    ts automation and config parcel-bundle tool

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+console.log("hello new programmer");
+
+let count: number = 33;
+console.log("count: ", count);
---
+++ src/shop/shop.ts
@@ -0,0 +1,6 @@
+console.log("hello new shopper");
+
+let count: number = 2025;
+console.log("count: ", count);
+
+export {}
